57afba97da9029f6399f1072846862be134b2293
Sithara sewwanthi <sithara_s@3logixs.lk>
2017-08-17 18:27:47 +0530

    add / to webhook

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/',function (req,res) {
 })
 
 //Facebook
-app.get('/webhook',function (req,res) {
+app.get('/webhook/',function (req,res) {
     if(req.query['hub.verify_token']=== "blondiebytes"){
         res.send(req.query['hub.challange'])
     }
